1dfb02d73b0102baecd465c8fe53aac7093d09a5
Prem <premilakiran@yahoo.com>
2021-08-11 22:22:39 -0400

    created a shopping cart. Can add and delete items from the cart. used strapi to create the products

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cd81a174-2e7e-4f61-92a1-72d3ba3db98d'
+};
---
+++ cart.jsx
@@ -0,0 +1,168 @@
+const {
+  Card,
+  Accordion,
+  Button,
+  Container,
+  Row,
+  Col,
+  Image,
+  Input,
+} = ReactBootstrap;
+
+const { Fragment, useState, useEffect, useReducer } = React;
+
+const Cart = (props) => {
+  const { Card, Accordion, Button } = ReactBootstrap;
+  let data = props.location.data ? props.location.data : products;
+  console.log(`data:${JSON.stringify(data)}`);
+
+  return <Accordion defaultActiveKey="0">{list}</Accordion>;
+};
+
+const Products = () => {
+
+  const [items, setItems] = React.useState([]);
+  const [cart, setCart] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
+
+  const [query, setQuery] = useState("http://localhost:1337/products");
+  
+  useEffect(async () => {
+
+    const rawData = await fetch(query);
+    const data = await rawData.json();
+    setItems(data);
+
+  }, []);
+
+  const addToCart = (e) => {
+    let name = e.target.name;
+    let item = items.filter((item) => item.name == name);
+    if (item[0].instock ==0) return;
+    item[0].instock = item [0].instock - 1
+    console.log(`add to Cart ${JSON.stringify(item)}`);
+    setCart([...cart, ... item]);
+  };
+
+
+    const deleteCartItem = (delIndex) => {
+    let newCart = cart.filter((item, i) => delIndex != i);
+    let target = cart.filter((item,index)=> delIndex ==index);
+    let newItems =items.map((item,index) =>{
+      if (item.name ==target[0].name) item.instock = item.instock + 1;
+      console.log(`delete Cart ${JSON.stringify(item)}`);
+      return item;
+    });
+
+    setCart(newCart);
+    setItems(newItems);
+   };
+
+  const photos = ["apple.png", "orange.png", "beans.png", "cabbage.png"];
+//here
+  let list = items.map((item, index) => {
+    let n = index + 1049 + Math.floor(Math.random() * 10);
+    let url = "https://picsum.photos/id/" + n + "/50/50";
+
+    return (
+      <li key={index}>
+        <Image src={url} width={70} roundedCircle></Image>
+        <Button variant="primary" size="large">
+          {item.name} : ${item.cost} : {item.instock} in stock
+        </Button>
+        <input name={item.name} type="submit" onClick={addToCart}></input>
+      </li>
+    );
+  });
+
+  let cartList = cart.map((item, index) => {
+    return (
+      <Card key={index}>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey={1 + index}>
+            {item.name}
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse
+          onClick={() => deleteCartItem(index)
+          }
+        
+          eventKey={1  + index}
+        >
+          <Card.Body>
+            $ {item.cost} from {item.country}
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    );
+  });
+
+  let finalList = () => {
+    let total = checkOut();
+    let final = cart.map((item, index) => {
+      return (
+        <div key={index} index={index}>
+          {item.name}
+        </div>
+      );
+    });
+    return { final, total };
+  };
+
+  const checkOut = () => {
+    let costs = cart.map((item) => item.cost);
+    const reducer = (accum, current) => accum + current;
+    let newTotal = costs.reduce(reducer, 0);
+    console.log(`total updated to ${newTotal}`);
+    return newTotal;
+  };
+
+  const restockProducts = (url) => {
+    doFetch(url);
+    let newItems = data.map((item) => {
+      let { name, country, cost, instock } = item;
+      return { name, country, cost, instock };
+    });
+    setItems([...items, ...newItems]);
+  
+  };
+
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Product List</h1>
+          <ul style={{ listStyleType: "none" }}>{list}</ul>
+        </Col>
+        <Col>
+          <h1>Cart Contents</h1>
+          <Accordion>{cartList}</Accordion>
+        </Col>
+        <Col>
+          <h1>CheckOut </h1>
+          <Button onClick={checkOut}>CheckOut $ {finalList().total}</Button>
+          <div> {finalList().total > 0 && finalList().final} </div>
+        </Col>
+      </Row>
+      <Row>
+        <form
+          onSubmit={(event) => {
+            restockProducts(`http://localhost:1337/${query}`);
+            console.log(`Restock called on ${query}`);
+            event.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <button type="submit">ReStock Products</button>
+        </form>
+      </Row>
+    </Container>
+  );
+};
+
+ReactDOM.render(<Products />, document.getElementById("root"));
